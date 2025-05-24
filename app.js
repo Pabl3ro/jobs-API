dfa4898059de0df2200a400eeee8a9572fe05837
Pablo Albanese <pabloalbanese15@gmail.com>
2025-05-24 20:09:23 -0300

    Swagger docs added

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 //Connect DB
 const connectDB = require("./db/connect");
@@ -41,8 +45,11 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res)=>{
-  res.send('jobs API');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 app.use('/api/v1/auth', authRouter);
